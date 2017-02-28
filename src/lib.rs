ffc26b315b4abfba220e2b4b3a0176d3db9b0fe1
Vitor Cortez <vitor.a.cortez@gmail.com>
2017-02-28 11:08:57 -0300

    Update `lib`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,32 @@
-mod blueprint;
+//! A <b>f</b>uzzy <b>i</b>nference <b>e</b>ngine library.
+//!
+//! This crate aims to offer a simple declarative API to build fuzzy inference engines.
+//!
+//! ## Examples
+//!
+//! Start by defining a `Blueprint` inference engine:
+//!
+//! ```rust
+//! # #![allow(unused_variables)]
+//! use fiers::blueprint::MaxMin;
+//!
+//! let blueprint = MaxMin::default();
+//! ```
+
+#![warn(missing_docs)]
+
+pub mod fuzzy;
+pub mod blueprint;
+pub mod util;
 
 pub use blueprint::Blueprint;
 
 #[cfg(test)]
 mod tests {
-  use super::Blueprint;
+  use super::blueprint::{MaxMin};
 
   #[test]
   fn test() {
-    println!("{:?}", Blueprint{})
+    println!("{:?}", MaxMin::default());
   }
 }
